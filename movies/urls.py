2d156299090d991dd41dac85f7f7dfad4ec345da
toneerav@gmail.com <toneerav@gmail.com>
2020-04-27 17:18:07 +0530

    Blank README added

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
+
 # from .views import GenreAPI
 
 urlpatterns = [
     # url(r'^content/?$', ContentAPI.as_view()),
-    url(r'^genre/?$', views.article_list)
+    url(r'^genre/?$', views.article_list),
+    path('detail/<int:pk>/', views.genre_detail)
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.http import Http404, JsonResponse
+from django.http import Http404, JsonResponse, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -46,3 +46,31 @@ def article_list(request):
             serializer.save()  # save() will call serializer's create() method
             return JsonResponse(serializer.data, safe=False, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+def genre_detail(request, pk):
+    try:
+        genre = Genre.objects.get(pk=pk)
+    except Genre.DoesNotExist:
+        '''
+        What is DoesNotExist ?
+        This exception is raised by the ORM in a couple places, 
+        for example by QuerySet.get() when an object is not found 
+        for the given query parameters.
+        https://docs.djangoproject.com/en/3.0/ref/models/instances/#django.db.models.Model.DoesNotExist
+        '''
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = GenreSerializer(genre)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        request_data = JSONParser().parse(request)
+        serializer = GenreSerializer(genre, data=request_data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        genre = genre.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
